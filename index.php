12674a3d861c56f836d829672a741b621432bcab
ABHIJIT MONDAL <70851067+abhijitmondal121@users.noreply.github.com>
2021-10-01 21:50:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,11 @@ $servername="localhost";
 $username="root";
 $password="";
 $database="test";
-
 $conn= mysqli_connect($servername,$username,$password,$database);
+
+
+
+
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $details = $_POST['details'];
