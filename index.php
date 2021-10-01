905f1f9d3211e24ff936a5490c66fbfb78062075
ABHIJIT MONDAL <70851067+abhijitmondal121@users.noreply.github.com>
2021-10-01 21:50:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ $database="test";
 $conn= mysqli_connect($servername,$username,$password,$database);
 
 
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $details = $_POST['details'];
     $image=$_FILES['photo']['name'];
